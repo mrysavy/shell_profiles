4e1195b5f381dd1791dd7c4c41a66e5a0d6bf990
Ryšavý Michal <mrysavy@trask.cz>
2017-07-19 12:19:18 +0000

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,12 +1,19 @@
+function run_dkrm-unset() {
+  unset DOCKER_TLS_VERIFY DOCKER_HOST DOCKER_CERT_PATH DOCKER_MACHINE_NAME
+  unalias docker
+  unalias docker-compose
+}
+
 function run_dkrm-set() {
   eval "$(docker-machine env $1)"
+  alias docker='docker $(docker-machine config $(docker-machine active))'
+  alias docker-compose='docker-compose $(docker-machine config $(docker-machine active))'
 }
-alias dkrm-set=run_dkrm-set;
 
 function run_dkrm-run() {
   docker $(docker-machine config $1) $2
 }
+
+alias dkrm-unset=run_dkrm-unset;
+alias dkrm-set=run_dkrm-set;
 alias dkrm-run=run_dkrm-run;
-
-alias dkrm='docker $(docker-machine config $(docker-machine active))'
-
